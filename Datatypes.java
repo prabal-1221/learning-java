646d26452071ac64c12da4eaadd1b0aeb48504af
prabal-1221 <mauryaprabal46@gmail.com>
2025-07-07 22:22:00 +0530

    some basic stuff

---
+++ Datatypes.java
@@ -0,0 +1,32 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int age = 10;
+        long phone = 1000l;
+
+        System.out.println(age);
+        System.out.println(phone);
+
+        float salary = 100.001f;
+        double big_salary = 100.001;
+
+        System.out.println(salary);
+        System.out.println(big_salary);
+
+        char initial = 'P';
+        System.out.println(initial);
+
+        String name = "Prabal";
+        System.out.println(name);
+
+        boolean flag = true;
+        System.out.println(flag);
+
+        /*
+        **naming conventions**
+        - case sensitive
+        - _, $, 0-9, A-Z, a-z
+        - start: _, $, A-Z, a-z
+        - camel case
+        */
+    }
+}
